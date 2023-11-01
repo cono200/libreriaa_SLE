6407e56dda4b88b8204cca448f3cb5910a43ec4a
cono200 <tatattian04@gmail.com>
2023-11-01 09:24:55 -0700

    Agregar archivos de proyecto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace libreriaa_SLE.Data
+{
+    public class AppDbContext:DbContext
+    {
+
+
+
+
+    }
+}
---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace libreriaa_SLE
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
